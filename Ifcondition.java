20a78cede386671f8562d30ad0ee85030973f671
Arun Kumar.R <146862202+ARUNKUMARRAMESHKUMAR@users.noreply.github.com>
2025-06-22 17:31:40 +0530

    Add files via upload

---
+++ Ifcondition.java
@@ -0,0 +1,96 @@
+package conditions;
+import java.util.Scanner;
+import java.io.*;
+public class ifcondition {
+
+	public static void main(String[] args) {
+		/*// TODO Auto-generated method stub
+		int a,b;
+		a=10;
+		b=23;
+		if(a<=b)
+		{
+			System.out.println("b is greater then a");
+		}
+		if(a>=b)
+		{
+			System.out.println("a is greater then b");
+		}
+}
+}
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class arun
+{
+public static void main(String args [])
+{
+		int a1,a2,a3;
+		a1=10;
+		a2=28;
+		a3=10;
+		if(a1==a2)
+		{
+			System.out.println(" a1 and a2 is equal ");
+		}
+		if(a1==a3)
+		{
+			System.out.println(" a1 and a3 is equal");
+		}
+		if(a2==a3)
+		{
+			System.out.println(" a2 and a3 is equal");		
+
+		}
+}
+}
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+import java.util.Scanner;
+class IfElse
+{
+public static void main(String args [])
+String oname="arunkumar";
+int oage=21;
+Scanner s=new Scanner();
+
+System.out.print("Enter your name :");
+String name=s.nextLine();
+
+System.out.print("Enter your age :");
+int age=s.nextInt();
+
+if(oname==name)
+{
+System.out.println(name+"name is correct");
+}
+
+if(oname!=name)
+{
+System.out.println(name+"name is not correct");
+}
+
+if(oage==age)
+{
+System.out.println(age+" age is correct");
+}
+
+
+if(oage!=age)
+{
+System.out.println(age+" age is not correct");
+}
+
+
+}		 
+
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
