c51133cfb419b7ec45faf8eeeeaf1d33d26db556
Arun Kumar.R <146862202+ARUNKUMARRAMESHKUMAR@users.noreply.github.com>
2025-06-25 23:07:34 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,64 @@
+
+
+
+
+
+ for(int i=0;i<stars.length;i++)
+ {
+             for(int j=0;j<stars[i].length;j++)
+         {
+             System.out.print('*');
+         }
+         System.out.println();
+ }
+     }
+}
+
+
+
+
+
+
+for(int i=row;i<stars.length;i--)
+{
+            for(int j=row;j<stars[i].length;j--)
+        {
+            System.out.print('*');
+        }
+        System.out.println();
+}
+    }
+}
+
+
+
+
+for(int i=o;i<=10;i++)
+{
+if(i!=10)
+{
+System.out.println(i);
+}
+}
+
+
+
+import java.util.Scanner;
+class arun
+{
+public static void main(String args [])
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter your tables number :");
+int table=s.nextInt();
+
+System.out.println("how many long do you want ?");
+int size=s.nextInt();
+
+for(i=i;i<=size;i++)
+{
+int mul=i*table;
+System.out.println(i+"x"+table+"="mul);
+}
+}
+}
